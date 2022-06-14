0a8d5f29f7515fb5527206855ca508caf038f37d
ascheidt <alessa.von.scheidt@cologne-intelligence.de>
2022-06-14 22:38:24 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+      static: {
+          directory: path.join(__dirname, 'dist'),
+      },
+      port: 3000
+  }
+};
