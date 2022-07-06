db407d3f8d4dde265c0dfc1a399868941bcdca62
ascheidt <alessa.von.scheidt@cologne-intelligence.de>
2022-07-06 09:34:45 +0200

    outsourced submit function into submit.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import './reset.css';
+import './submit';
 import { getTodosFromMyStorage } from './local-storage';
 import { Todo } from './Todo';
-import { addTodo, renderTodos } from './todos';
-
-const text = document.getElementById('text') as HTMLInputElement;
+import { renderTodos } from './todos';
 
 const todos: Todo[] = getTodosFromMyStorage();
-
-// Submit
-const handleSubmit = () => {
-  if (text?.value === '') return;
-
-  addTodo(text.value);
-  text.value = '';
-};
-
-document.getElementById('submit')?.addEventListener('click', handleSubmit);
 
 // Render existing todos
 renderTodos(todos);
---
+++ src/submit.ts
@@ -0,0 +1,12 @@
+import { addTodo } from './todos';
+
+const text = document.getElementById('text') as HTMLInputElement;
+
+const handleSubmit = () => {
+  if (text?.value === '') return;
+
+  addTodo(text.value);
+  text.value = '';
+};
+
+document.getElementById('submit')?.addEventListener('click', handleSubmit);
