e4771da0af4933c94ae6aa6bc6a2e7c182c3abc8
ascheidt <alessa.von.scheidt@cologne-intelligence.de>
2022-07-24 23:52:04 +0200

    updated styles

--- src/components/atoms/init.ts
+++ src/components/atoms/init.ts
@@ -1,7 +1,7 @@
 import { addNewTodo, closeModal, openModalCallback } from '../../helpers/modal';
 
 export const init = () => {
-  const addButton = document.getElementById('openModal');
+  const addButton = document.getElementById('open-modal');
   addButton?.addEventListener('click', openModalCallback);
 
   document.getElementById('modal')?.addEventListener('click', closeModal);
