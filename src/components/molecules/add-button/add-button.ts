e4374b3ad35f0716386762d3a119f9b38261a144
ascheidt <alessa.von.scheidt@cologne-intelligence.de>
2022-08-21 16:50:42 +0200

    updated icon color and button styles

--- src/components/molecules/add-button/add-button.ts
+++ src/components/molecules/add-button/add-button.ts
@@ -1,16 +1,17 @@
 import { generateButton } from '../../atoms/button/button';
 import { openModalCallback } from '../modal/modal';
-import plus from '../../../icons/plus.svg';
+// import plus from '../../../icons/plus.svg';
+import plusIcon from '../../../icons/plusIcon.svg';
 
 export const renderAddButton = () => {
   const body = document.querySelector('body');
   const plusImage = document.createElement('img');
-  plusImage.src = plus;
+  plusImage.src = plusIcon;
 
   const addButton = generateButton({
     content: plusImage,
     onClick: () => openModalCallback(),
-    cssClasses: ['button', 'button--rounded', 'button--border', 'button__image', 'button__image--padding'],
+    cssClasses: ['button', 'button--rounded', 'button--border', 'button__image', 'button__image--padding', 'button__image--larger'],
   });
 
   body?.appendChild(addButton);
